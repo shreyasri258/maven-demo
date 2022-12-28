d3e77a65483b7727413b749a08131af4479e1658
A Shreya Sri <84087589+shreyasri258@users.noreply.github.com>
2022-12-28 21:42:07 +0530

    Add files via upload

---
+++ src/main/java/com/app/demo/se/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.se;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
